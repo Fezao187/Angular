2d0759aa824371fbb9164723487b167c3ebb158e
Fezao187 <katlegomaredi9@gmail.com>
2025-08-23 22:29:14 +0200

    angular router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,8 @@ import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  standalone:true,
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
