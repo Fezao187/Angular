721112a05c97a545ba4090493f0943c48ccb2f28
Fezao187 <katlegomaredi9@gmail.com>
2025-08-23 22:50:34 +0200

    Style the navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,16 @@
 import { Routes } from '@angular/router';
 export const routes: Routes = [{
     path: '',
-    pathMatch:'full',
-    loadComponent:()=>{
+    pathMatch: 'full',
+    loadComponent: () => {
         return import('./pages/home/home.component')
-        .then((m)=>m.HomeComponent)
+            .then((m) => m.HomeComponent)
     }
 },
-{path:'todos',
-    loadComponent:()=>{
+{
+    path: 'todos',
+    loadComponent: () => {
         return import('./pages/todos/todos.component')
-        .then((m)=>m.TodosComponent)
+            .then((m) => m.TodosComponent)
     }
 }];
